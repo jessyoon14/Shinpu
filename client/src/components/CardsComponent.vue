31f64546ab57f1b2f0970ff18fff3661b1300604
JeesooYoon <jeesooyoon@kaist.ac.kr>
2019-12-01 03:29:22 +0900

    finished shuffle animation

--- client/src/components/CardsComponent.vue
+++ client/src/components/CardsComponent.vue
@@ -1,253 +1,134 @@
 <template>
-  <div id="app">
-    <div class="title-section">
-      <h1 class="title">
-        <img class="vue" src="https://vuejs.org/images/logo.png">
-        Card Shuffling
-      </h1>
-
-      <div class="speed-buttons">
-        <a
-          class="button is-small"
-          :class="[{ 'is-outlined': shuffleButtonActive === 0, 'is-light': shuffleButtonActive != 0 }]"
-          @click="selectShuffleSpeed(0)"
-        >Slow</a>
-        <a
-          class="button is-small"
-          :class="[{ 'is-outlined': shuffleButtonActive === 1, 'is-light': shuffleButtonActive != 1 }]"
-          @click="selectShuffleSpeed(1)"
-        >Medium</a>
-        <a
-          class="button is-small"
-          :class="[{ 'is-outlined': shuffleButtonActive === 2, 'is-light': shuffleButtonActive != 2 }]"
-          @click="selectShuffleSpeed(2)"
-        >Fast</a>
-      </div>
-      <div class="main-buttons">
-        <transition name="fade">
-          <a v-if="shuffledDeck" @click="displayInitialDeck" class="button is-primary is-outlined">
-            Reset&nbsp;&nbsp;
-            <i class="fas fa-undo"></i>
-          </a>
-        </transition>
-        <transition :name="shuffleSpeed">
-          <a @click="shuffleDeck" class="button is-primary">
-            Shuffle&nbsp;&nbsp;
-            <i class="fas fa-random"></i>
-          </a>
-        </transition>
-      </div>
-      <transition name="fade">
-        <div v-if="shuffleCount" class="count-section"># of Shuffles: {{ shuffleCount }}</div>
-      </transition>
-    </div>
-    <p>Hello</p>
-    <div class="deck">
+  <div id="cardgrid">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css"
+    >
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
+    <h1 class="title">
+      <img class="vue-logo" src="https://vuejs.org/images/logo.png">
+      Card Shuffling
+    </h1>
+    <!-- <div class="deck">
       <div class="card">
         <span class="card__suit card__suit--top">♣</span>
         <span class="card__number">A</span>
         <span class="card__suit card__suit--bottom">♣</span>
       </div>
+    </div>-->
+    <div class="main-buttons">
+      <button @click="shuffleDeck" class="button is-primary">
+        Shuffle
+        <i class="fas fa-random"></i>
+      </button>
     </div>
-
     <transition-group :name="shuffleSpeed" tag="div" class="deck">
-      <div
-        v-for="card in cards"
-        :key="card.id"
-        class="card"
-        :class="{ 'black': card.suit === '♠' || card.suit ===  '♣',
-                        'red': card.suit === '♥' || card.suit ===  '♦' }"
-      >
-        <span class="card__suit card__suit--top">{{ card.suit}}</span>
-        <span class="card__number">{{ card.name}}</span>
-        <span class="card__suit card__suit--bottom">{{ card.suit}}</span>
+      <div v-for="card in cards" :key="card.id" class="card" :class="suitColor[card.suit]">
+        <span class="card__suit card__suit--top">{{ card.suit }}</span>
+        <span class="card__number">{{ card.rank }}</span>
+        <span class="card__suit card__suit--bottom">{{ card.suit }}</span>
       </div>
     </transition-group>
-    <a href="https://twitter.com/djirdehh" target="_blank" class="twitter-section">
-      <i class="fab fa-twitter-square"></i>
-    </a>
   </div>
 </template>
 
 <script>
-import v1 from "../main.js";
 //import Vue from "vue";
-// export default {
-//   name: "CardsComponent",
-//   data() {
-//     return {
-//       vue: 0
-//     };
-//   }
-// };
-// this.vue = new Vue({
-//   el: "#app",
-
-//   data: {
-//     names: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
-//     suits: ["♥", "♦", "♠", "♣"],
-//     cards: [],
-//     shuffleButtonActive: 1,
-//     shuffleSpeed: "shuffleMedium",
-//     shuffleCount: 0,
-//     shuffledDeck: false
-//   },
-//   created() {
-//     this.displayInitialDeck();
-//   },
-//   methods: {
-//     displayInitialDeck() {
-//       let id = 1;
-//       this.cards = [];
-//       for (let s = 0; s < this.suits.length; s++) {
-//         for (let n = 0; n < this.names.length; n++) {
-//           let card = {
-//             id: id,
-//             value: n + 1,
-//             name: this.names[n],
-//             suit: this.suits[s]
-//           };
-//           this.cards.push(card);
-//           id++;
-//         }
-//       }
-
-//       this.shuffledDeck = false;
-//       this.shuffleCount = 0;
-//       return this.cards;
-//     },
-//     selectShuffleSpeed(n) {
-//       this.shuffleButtonActive = n;
-
-//       if (n === 0) this.shuffleSpeed = "shuffleSlow";
-//       if (n === 1) this.shuffleSpeed = "shuffleMedium";
-//       if (n === 2) this.shuffleSpeed = "shuffleFast";
-//     },
-//     shuffleDeck() {
-//       let counter = this.cards.length;
-
-//       while (counter > 0) {
-//         let randomIndex = Math.floor(Math.random() * counter);
-
-//         counter--;
-
-//         let temp = this.cards[counter];
-//         Vue.set(this.cards, counter, this.cards[randomIndex]);
-//         Vue.set(this.cards, randomIndex, temp);
-//       }
-
-//       this.shuffledDeck = true;
-//       this.shuffleCount++;
-//     }
-//   }
-// });
-
+import Vue from "../main.js";
 export default {
   name: "CardsComponent",
   data() {
     return {
-      names: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+      ranks: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
       suits: ["♥", "♦", "♠", "♣"],
       cards: [],
-      shuffleButtonActive: 1,
-      shuffleSpeed: "shuffleMedium",
-      shuffleCount: 0,
-      shuffledDeck: false
+      suitColor: {
+        "♠": "black",
+        "♣": "black",
+        "♦": "red",
+        "♥": "red"
+      },
+      shuffleSpeed: "shuffleMedium"
     };
   },
-  create() {
+  created() {
     this.displayInitialDeck();
   },
   methods: {
     displayInitialDeck() {
       let id = 1;
       this.cards = [];
+
       for (let s = 0; s < this.suits.length; s++) {
-        for (let n = 0; n < this.names.length; n++) {
+        for (let r = 0; r < this.ranks.length; r++) {
           let card = {
             id: id,
-            value: n + 1,
-            name: this.names[n],
+            rank: this.ranks[r],
             suit: this.suits[s]
           };
           this.cards.push(card);
           id++;
         }
       }
-
-      this.shuffledDeck = false;
-      this.shuffleCount = 0;
-      return this.cards;
-    },
-    selectShuffleSpeed(n) {
-      this.shuffleButtonActive = n;
-
-      if (n === 0) this.shuffleSpeed = "shuffleSlow";
-      if (n === 1) this.shuffleSpeed = "shuffleMedium";
-      if (n === 2) this.shuffleSpeed = "shuffleFast";
     },
     shuffleDeck() {
-      let counter = this.cards.length;
-      while (counter > 0) {
-        let randomIndex = Math.floor(Math.random() * counter);
+      for (let i = this.cards.length - 1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * i);
 
-        counter--;
-
-        let temp = this.cards[counter];
-        v1.set(this.cards, counter, this.cards[randomIndex]);
-        v1.set(this.cards, randomIndex, temp);
+        let temp = this.cards[i];
+        Vue.$set(this.cards, i, this.cards[randomIndex]);
+        Vue.$set(this.cards, randomIndex, temp);
       }
-
-      this.shuffledDeck = true;
-      this.shuffleCount++;
     }
   }
 };
+// window.onload = function() {
+//   new Vue({
+//     el: "#cardgrid"
+//   });
+// };
 </script>
 
-<style lang="scss" scoped>
+
+
+<style scoped>
 @import url("https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700");
-@import url("https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css");
-@import url("https://use.fontawesome.com/releases/v5.0.6/css/all.css");
-$large: 1210px;
-$medium: 768px;
 
 html,
 body,
-#app {
+#cards {
   height: 100%;
   background: ghostwhite;
 }
 
-.title-section {
+.title {
   font-family: Roboto Slab, sans-serif;
   text-align: center;
-  margin-top: 30px;
-}
-
-.title-section .title {
+  padding-top: 30px;
+  margin-bottom: 0 !important;
   font-weight: 300;
   font-size: 3rem;
 }
 
-.title-section .vue {
+.vue-logo {
   height: 55px;
   position: relative;
   top: 10px;
 }
 
 .speed-buttons {
-  padding-bottom: 30px;
-  .button {
-    height: 2.5em;
-  }
+  text-align: center;
+  padding-top: 30px;
+}
+.speed-buttons .button {
+  height: 2.5em;
 }
 
 .main-buttons {
   display: block;
   margin: 0 auto;
-  padding-bottom: 30px;
   text-align: center;
+  padding-top: 30px;
   margin-bottom: 0 !important;
 }
 
@@ -257,8 +138,13 @@ body,
   right: 10px;
 }
 
+.fas {
+  padding-left: 5px;
+}
+
 .deck {
   margin-left: 30px;
+  padding-top: 30px;
 }
 
 .card {
@@ -289,10 +175,12 @@ body,
 }
 
 .card__number {
-  width: 100%;
+  text-align: left;
+  padding-left: 5px;
+  /* width: 100%;
   position: absolute;
   top: 38%;
-  text-align: center;
+  text-align: center; */
 }
 
 .red {
@@ -307,14 +195,12 @@ body,
   position: absolute;
   bottom: 10px;
   right: 10px;
-
-  .fa-twitter-square {
-    color: #00d1b2;
-    font-size: 30px;
-  }
+}
+.twitter-section .fa-twitter-square {
+  color: #00d1b2;
+  font-size: 30px;
 }
 
-// Transitions
 .shuffleSlow-move {
   transition: transform 2s;
 }
@@ -331,13 +217,13 @@ body,
 .fade-leave-active {
   transition: opacity 0.5s;
 }
+
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
 }
 
-// Media
-@media (max-width: $large) {
+@media (max-width: 1210px) {
   .deck {
     position: initial;
     top: 0;
@@ -348,7 +234,3 @@ body,
   }
 }
 </style>
-
-
-
-
--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-const v1 = new Vue({
+// const v1 = new Vue({
+//   render: h => h(App),
+// }).$mount('#app');
+
+// export default v1;
+
+export default new Vue({
   render: h => h(App),
 }).$mount('#app');
-
-export default v1;
-
