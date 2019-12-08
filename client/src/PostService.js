4426955139b7f3810ac353fe12bf4b3a7d4cb639
JeesooYoon <jeesooyoon@kaist.ac.kr>
2019-12-08 21:29:42 +0900

    fix asset path & add productin specific api endpoint

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000/members/';
+const url = process.env.NODE_ENV === 'production' ?
+    'http://ssal.sparcs.org:14000/members'
+    :
+    'http://localhost:8000/members/';
 
 class PostService {
     //get Posts
